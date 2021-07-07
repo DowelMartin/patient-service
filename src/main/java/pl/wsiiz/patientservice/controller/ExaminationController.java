1b3dcace4ce751daf4c312c28de9991065226871
DowelMartin <martindowelofficial@gmail.com>
2021-07-07 17:02:50 +0200

    Controllers changes&fixes

--- src/main/java/pl/wsiiz/patientservice/controller/ExaminationController.java
+++ src/main/java/pl/wsiiz/patientservice/controller/ExaminationController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import pl.wsiiz.patientservice.model.Examination;
 import pl.wsiiz.patientservice.service.ExaminationService;
 
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 @Controller
@@ -19,9 +20,11 @@ public class ExaminationController extends BaseController<Examination> {
     @Override
     public List<List<String>> list() {
         {
-            functionsMap.put("Data", exam->exam.getDate().toString());
+            functionsMap.put("Data", exam-> new SimpleDateFormat("dd-MM-yyy").format(exam.getDate()));
             functionsMap.put("Doktor", Examination::getDoctor);
             functionsMap.put("Opis", Examination::getDescription);
+            functionsMap.put("Typ", Examination::getType);
+            functionsMap.put("Miejsce", Examination::getExaminationPlace);
             return convertForListing(service.getAll(), functionsMap);
         }
     }
--- src/main/java/pl/wsiiz/patientservice/controller/ReferralController.java
+++ src/main/java/pl/wsiiz/patientservice/controller/ReferralController.java
@@ -3,25 +3,28 @@ package pl.wsiiz.patientservice.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.wsiiz.patientservice.model.Examination;
+import pl.wsiiz.patientservice.model.Referral;
 import pl.wsiiz.patientservice.service.ExaminationService;
+import pl.wsiiz.patientservice.service.ReferralService;
 
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 @Controller
 @RequestMapping("/referrals")
-public class ReferralController extends BaseController<Examination> {
-    private final ExaminationService service;
+public class ReferralController extends BaseController<Referral> {
+    private final ReferralService service;
 
-    public ReferralController(ExaminationService service) {
+    public ReferralController(ReferralService service) {
         this.service = service;
     }
 
     @Override
     public List<List<String>> list() {
         {
-            functionsMap.put("Data", exam->exam.getDate().toString());
-            functionsMap.put("Doktor", Examination::getDoctor);
-            functionsMap.put("Opis", Examination::getDescription);
+            functionsMap.put("Data wygasniecia", s -> new SimpleDateFormat("dd-MM-yyy").format(s.getExpirationDate()));
+            functionsMap.put("Doktor", Referral::getDoctor);
+            functionsMap.put("Nazwa badania", Referral::getExaminationName);
             return convertForListing(service.getAll(), functionsMap);
         }
     }
